c1e8d914fbe8cbaa075b74dac2ab7a1fad04d287
pyroblaster <starac@thegame.com>
2018-04-05 19:48:50 +0200

    comparing

--- src/mediumBoolean.kt
+++ src/mediumBoolean.kt
@@ -9,4 +9,15 @@ fun main(args: Array<String>) {
     characterAlive=false //changing value of characterAlive
     println("Is character alive: $characterAlive.")
     //again printing value of characterAlive, should print "Is character alive: false."
+
+
+    //comparing values
+    val oneGreaterThanTwo:Boolean=(1>2) 
+    println("Is 1>2? $oneGreaterThanTwo.")
+
+    val oneEqualTwo:Boolean=(1==2)
+    println("Is 1==2? $oneEqualTwo.")
+
+    val oneLesserThanTwo:Boolean=(1<2)
+    println("Is 1<2? $oneLesserThanTwo.")
 }
