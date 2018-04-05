357531601a3d8cb5baebcc8db59c27f7edc74306
pyroblaster <starac@thegame.com>
2018-04-05 21:04:12 +0200

    array

---
+++ src/mediumArray.kt
@@ -0,0 +1,10 @@
+/**
+ * Created by Reptilko on 4/5/2018.
+ */
+fun main(args: Array<String>) {
+    var carBrands = arrayOf("Lamborghini", "Ferrari", "Porsche")  //array of strings;declaration of array using arrayOf
+    println("What do you want to buy? ${carBrands[0]}")  //should print "Lamborghini"
+
+    var fibonnaci = arrayOf(1,1,2,3,5,8,13,21,34)  //array of numbers
+    println("Second Fibonnaci number: ${fibonnaci[1]}")  //should print "1"
+}
